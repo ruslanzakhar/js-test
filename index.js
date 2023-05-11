8a8ab95a36b1b8e260f15ef2d4412b253e8d9780
ruslanzakhar <zakharchuk_ruslan@icloud.com>
2023-05-11 19:59:14 +0300

    Update index.js

--- index.js
+++ index.js
@@ -62,16 +62,75 @@
 //       :(result = false);
 //     // Change code above this line
 //     return result;
-function withdraw(amount, balance) {
-    if (amount === 0) {
-      console.log("Для проведення операції введіть суму більшу за нуль");
-    } else if (amount > balance) {
-      console.log("Недостатньо коштів на рахунку");
-    } else {
-      console.log("Операція зняття коштів проведена успішно");
-    }
-  }
+// function withdraw(amount, balance) {
+//     if (amount === 0) {
+//       console.log("Для проведення операції введіть суму більшу за нуль");
+//     } else if (amount > balance) {
+//       console.log("Недостатньо коштів на рахунку");
+//     } else {
+//       console.log("Операція зняття коштів проведена успішно");
+//     }
+//   }
   
-  withdraw(0, 300); // "Для проведення операції введіть суму більшу за нуль"
-  withdraw(500, 300); // "Недостатньо коштів на рахунку"
-  withdraw(100, 300); // "Операція зняття коштів проведена успішно"
+//   withdraw(0, 300); // "Для проведення операції введіть суму більшу за нуль"
+//   withdraw(500, 300); // "Недостатньо коштів на рахунку"
+//   withdraw(100, 300); // "Операція зняття коштів проведена успішно"
+
+
+// const book = {
+//   title: "The Last Kingdom",
+//   author: "Bernard Cornwell",
+//   genres: ["historical prose", "adventure"],
+//   rating: 8.38,
+// };
+
+
+
+// for (const key in book) {
+//   // Якщо це власна властивість - виконуємо тіло if
+//   if (book.hasOwnProperty(key)) {
+//     console.log(key);
+//     console.log(book[key]);
+//   }
+
+
+
+//   // Якщо це невласна властивість - нічого не робимо
+// }
+// const name = Object.create(book);
+// book.name = 'rusik'
+// console.log(book.hasOwnProperty('name'))
+
+
+
+// const temps = [14, -4, 25, 8, 11];
+
+// // В консолі буде масив
+// console.log(temps);
+// // ❌ Так не спрацює, тому що передаємо цілий масив
+// console.log(Math.max(temps)); // NaN
+
+// // В консолі буде набір окремих чисел
+// console.log(...temps);
+// // ✅ Розподілимо колекцію елементів у якості окремих аргументів
+// console.log(Math.max(...temps)); // 25
+
+
+
+
+const string = "Java Script";
+
+const letters = string.split('');
+let inverseString = '';
+
+for (const letter of letters){
+
+if (letter === letter.toLocaleLowerCase()){
+inverseString += letter.toLocaleUpperCase();
+}
+else{
+  inverseString += letter.toLocaleLowerCase();
+}
+
+}
+console.log(inverseString)
