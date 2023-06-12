e35f03d36425d3d349d5859ea6cad97109edca81
ruslanzakhar <zakharchuk_ruslan@icloud.com>
2023-06-12 20:55:27 +0300

    b

---
+++ index.js
@@ -0,0 +1,11 @@
+const book = {
+    title: "The Last Kingdom",
+    author: "Bernard Cornwell",
+    genres: ["historical prose", "adventure"],
+    rating: 8.38,
+  };
+  
+ for (key in book){
+console.log(key)
+
+ }
