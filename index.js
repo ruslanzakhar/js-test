f77f8c0574b2964c280259055dd54f1411bf18e6
ruslanzakhar <zakharchuk_ruslan@icloud.com>
2023-06-13 20:46:55 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,37 @@
-const book = {
-    title: "The Last Kingdom",
-    author: "Bernard Cornwell",
-    genres: ["historical prose", "adventure"],
-    rating: 8.38,
-  };
-  
- for (key in book){
-console.log(key)
+'use strict';
 
- }
+// const petya = {
+//   username: "Petya",
+//   showName() {
+//     console.log(petya.username);
+//   },
+// };
+
+// petya.showName();
+// console.log(window)
+
+// function foo() {
+//   console.log(this);
+// }
+
+// foo();
+
+const temps = [14, -4, 25, 8, 11];
+
+
+
+function findNumb(){
+  let arr = [];
+  for (const temp of temps){
+    
+    if (temp > 11){
+      
+    let bigNum = arr.push(temp);
+    
+    return console.log(arr)
+    
+  }
+
+}
+}
+findNumb(temps);
