8d6896d3fc57d7dfe09aed12063a89ea8601b645
ruslanzakhar <zakharchuk_ruslan@icloud.com>
2023-07-25 17:21:50 +0300

    Update index.js

--- index.js
+++ index.js
@@ -13,15 +13,7 @@
 // function foo() {
 //   console.log(this);
 // }
+const num1 = 8;
+const num2 = '10';
 
-function count(countFrom = 1, countTo = 5, step = 1) {
-  console.log(`countFrom = ${countFrom}, countTo = ${countTo}, step = ${step}`);
-
-  for (let i = countFrom; i <= countTo; i += step) {
-    console.log(i);
-  }
-}
-
-count(1, 5); // countFrom = 1, countTo = 5, step = 1
-count(2); // countFrom = 2, countTo = 10, step = 1
-count(); // countFrom = 0, countTo = 10, step = 1
+console.log(num1 ++ num2);
